376b83fb7cb90df72d4162feb29bc4a0073c6c72
camCy <ccamillo245@gmail.com>
2022-11-01 17:33:36 -0500

    Componentes Increase & Decrease

--- src/hooks/useContador.js
+++ src/hooks/useContador.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export const useContador = (initialState) => {
+export const useContador = (initialState, numToSum, numToRes) => {
     //use state
     const [value,setValue] = useState(initialState)
     //
@@ -8,13 +8,20 @@ export const useContador = (initialState) => {
     const modificarValor = (num) => {
          setValue(Math.max(value+num,0))
     }
-
+    const increaseBy = ()=>{
+        setValue(value+numToSum)
+    }
+    const decreaseBy = ()=>{
+        setValue(value-numToRes)
+    }
     const reset=()=>{
         setValue(initialState)
     }
     return {
         value,
         modificarValor,
-        reset
+        reset,
+        increaseBy,
+        decreaseBy
     }
 }
--- src/pages/FirstPage.jsx
+++ src/pages/FirstPage.jsx
@@ -1,15 +1,15 @@
 import { useContador} from "../hooks/useContador";
 
 export const FirstPage = () =>{
-    const{value, modificarValor, reset}=useContador(200)
+    const{value, increaseBy, reset, decreaseBy}=useContador(200,1,10)
     return(
         <>
         <h1>contador mejorado</h1>
         <hr />
         <h1>{value}</h1>
         
-        <button onClick={()=>modificarValor(+1)}> +1</button>
-        <button onClick={()=>modificarValor(-1)}> -1</button>
+        <button onClick={increaseBy}> incrementar</button>
+        <button onClick={decreaseBy}> decrementar</button>
         <button onClick={reset}>reset</button>
         </>
     )
